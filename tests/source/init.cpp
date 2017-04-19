c616e3cba3d261ec7d3944dce08df0888c98e78a
zhanchi5 <zhanchi5@outlook.com>
2017-04-19 20:20:49 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -4,28 +4,22 @@
 SCENARIO("insert") {
     Binary_Search_Tree<unsigned int> r;
     Binary_Search_Tree<unsigned int>* root;
-    int rv = r.Add(10, root);
+    int rv = r.insert(10, root);
     
     REQUIRE( rv == 10);
 }
-SCENARIO("print_tree") {
-    Binary_Search_Tree<unsigned int> r;
-    Binary_Search_Tree<unsigned int>* root;
-    int rv = r.print(11, root);
-    
-    REQUIRE( rv == 11);
-}
+
 SCENARIO("search") {
     Binary_Search_Tree<unsigned int> r;
     Binary_Search_Tree<unsigned int>* root;
-    int rv = r.Search(12, root);
+    int rv = r.search(12, root);
     
     REQUIRE( rv == 12);
 }
 SCENARIO("remove") {
     Binary_Search_Tree<unsigned int> r;
     Binary_Search_Tree<unsigned int>* root;
-    int rv = r.Search(13, root);
+    int rv = r.search(13, root);
     
     REQUIRE( rv == 13);
 }
