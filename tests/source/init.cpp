fe13c68facb56712758b442a3832dd70463d49df
zhanchi5 <zhanchi5@outlook.com>
2017-04-19 20:24:46 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -4,7 +4,7 @@
 SCENARIO("insert") {
     Binary_Search_Tree<unsigned int> r;
     Binary_Search_Tree<unsigned int>* root;
-    int rv = r.insert(10, root);
+    int rv = r.insert(10);
     
     REQUIRE( rv == 10);
 }
@@ -12,14 +12,14 @@ SCENARIO("insert") {
 SCENARIO("search") {
     Binary_Search_Tree<unsigned int> r;
     Binary_Search_Tree<unsigned int>* root;
-    int rv = r.search(12, root);
+    int rv = r.search(12);
     
     REQUIRE( rv == 12);
 }
 SCENARIO("remove") {
     Binary_Search_Tree<unsigned int> r;
     Binary_Search_Tree<unsigned int>* root;
-    int rv = r.search(13, root);
+    int rv = r.search(13);
     
     REQUIRE( rv == 13);
 }
