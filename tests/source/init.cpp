e3f8fd168c02cbd00f27225a494a66e440a50bc4
zhanchi5 <zhanchi5@outlook.com>
2017-04-20 11:19:34 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -7,22 +7,16 @@ Binary_Search_Tree<unsigned int> r;
 SCENARIO("search") {
     int a = int(10);
     int rv = r.insert(a);
-    int rs = r.search(10);
+    int get_request = r.search(10);
     
-    REQUIRE( rs == true);
+    REQUIRE( get_request == true);
 }
 
 SCENARIO("remove"){
     int a = int(10);
     int rv = r.insert(a);
     r.remove(a);
-    bool answer;
-    if (!r.search(10)){
-        answer = true;
-    }
-    else{
-        answer = false;
-    }
+    int answer = r.search(10)
     
-    REQUIRE( answer == true);
+    REQUIRE( answer == false);
 }
