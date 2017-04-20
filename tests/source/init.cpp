6740e66200242761428dc2d9230b7125ad8fd2ee
zhanchi5 <zhanchi5@outlook.com>
2017-04-20 10:56:23 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -12,3 +12,17 @@ SCENARIO("search") {
     REQUIRE( rs == true);
 }
 
+SCENARIO("remove"){
+    int a = int(10);
+    int rv = r.insert(a);
+    bool answer;
+    r.remove(10);
+    if (isEmpty()){
+        answer = true;
+    }
+    else{
+        answer = false;
+    }
+    
+    REQUIRE( answer == true);
+}
